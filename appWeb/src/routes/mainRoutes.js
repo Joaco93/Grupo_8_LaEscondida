dff1f933a114d88dfbce53a59c5ab55099a4dae9
Rocio Caballero <drcaballero25@gmail.com>
2021-10-01 20:42:34 -0300

    cambio de rutas

--- appWeb/src/routes/mainRoutes.js
+++ appWeb/src/routes/mainRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const mainController = require('../controllers/mainController');
+const mainController = require('../controllers/mainController')
 
 
-router.get('/', mainController.home);
+router.get('/home', mainController.home)
+router.get('/login', mainController.login);
+router.get('/register', mainController.register);
+router.get('/productCart', mainController.productCart)
+
 
 module.exports = router;
