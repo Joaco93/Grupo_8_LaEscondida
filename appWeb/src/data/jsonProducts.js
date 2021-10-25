7b36ab5f0354f8c83306849fc2c66e98c6550cb2
Rocio Caballero <drcaballero25@gmail.com>
2021-10-25 16:34:18 -0300

    edit and delete products

--- appWeb/src/data/jsonProducts.js
+++ appWeb/src/data/jsonProducts.js
@@ -19,5 +19,31 @@ module.exports = {
             products_db,
             JSON.stringify(data, null, 2)
         );
+        
     },
+    edit: function (data,file,id) {
+        const directory = path.resolve(__dirname,"../data","product.json")
+        let products = this.all();
+        products.map(product => {
+            if(product.id == id ){
+                product.name = parseInt(data.name),
+                product.description = data.description,
+                product.category = data.category,
+                product.size = data.size,
+                product.price = data.price
+                return product
+            }
+        })
+        fs.writeFileSync(directory,JSON.stringify(products,null,2));
+        return true;
+    },
+    delete: function (id) {
+        const directory = path.resolve(__dirname,"../data","product.json")
+        let productos = this.all();
+        let deleted = this.one(id);
+        fs.unlinkSync(path.resolve(__dirname,"../../public/uploads/products",deleted.image))
+        productos = productos.filter(producto => producto.id != deleted.id )
+        fs.writeFileSync(directory,JSON.stringify(productos,null,2));
+        return true;
+    }
 };
