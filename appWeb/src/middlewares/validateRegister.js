7f219fedc3837fa3af5706167bc3978a291abe48
Rocio Caballero <drcaballero25@gmail.com>
2022-01-08 22:31:49 -0300

    validaterRegister create

---
+++ appWeb/src/middlewares/validateRegister.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const { body } = require("express-validator");
+const db = require("../database/models");
+module.exports = [
+  body("name")
+    .notEmpty()
+    .withMessage("Escribe tu nombre y apellido")
+    .isLength({ min: 8 })
+    .withMessage("Debes poner mínimamente 8 caracteres"),
+  body("email")
+    .notEmpty()
+    .withMessage("Escribe tu correo electrónico")
+    .isEmail()
+    .withMessage("Debes escribir un formato de correo válido")
+    .custom((value, { req }) => {
+      return db.User.findOne({
+        where: {
+          email: req.body.email,
+        },
+      }).then((user) => {
+        if (user) {
+          return Promise.reject("E-mail already in use");
+        }
+      });
+    }),
+  body("password")
+    .notEmpty()
+    .withMessage("Tienes  que  escribir  una  contraseña")
+    .isLength({ min: 8 })
+    .withMessage("Tu contraseña debe tener mínimo 8 caracteres"),
+  body("image").custom((value, { req }) => {
+    let file = req.file;
+    let acceptedExtensions = [".jpg", ".png", ".gif", ".jpeg"];
+
+    if (!file) {
+      throw new Error("Tienes que subir una imagen");
+    } else {
+      let fileExtension = path.extname(file.originalname);
+      if (!acceptedExtensions.includes(fileExtension)) {
+        throw new Error(
+          `Las extensiones de archivo permitidas son ${acceptedExtensions.join(
+            ", "
+          )}`
+        );
+      }
+    }
+
+    return true;
+  }),
+];
