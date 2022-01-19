cb23453544ee73409a425a19635e70a1e04ba03a
Rocio Caballero <drcaballero25@gmail.com>
2022-01-19 12:21:11 -0300

    modificando el login

--- appWeb/src/controllers/userController.js
+++ appWeb/src/controllers/userController.js
@@ -1,6 +1,9 @@
 const path = require("path");
 const fileJson = require('../data/jsonUsers');
 const bcrypt = require('bcrypt');
+const db = require('../database/models');
+
+const {validationResult} = require("express-validator")
 
 const controlador = {
     // Formulario de registro
@@ -8,67 +11,55 @@ const controlador = {
         res.render(path.resolve(__dirname, "../views/users/register.ejs"))
     },
 
-    //Formulario de login
-    login: (req, res) => {
-        res.render(path.resolve(__dirname, "../views/users/login.ejs"))
-    },
-    createUser: (req, res) => {
+    processLogin: (req, res) => {
+        let errores = validationResult(req);
+        const { email, pass, recordar } = req.body;
 
-        let users = fileJson.getUsers();
-        let newUser = {
-            id: 0,
-            name: req.body.name,
-            email: req.body.email,
-            password: bcrypt.hashSync(req.body.password, 10),
-            fechaDeNacimiento: req.body.birth,
-            avatar: ""
-        }
-       
-        if (users.length > 0) {
-            let lastId = users[users.length - 1].id;
-            lastId++;
-            newUser.id = lastId;
-        } else {
-            newUser.id++;
-        }
-
-        if (req.file) {
-            newUser.avatar = req.file.filename;
-        } 
-
-        users.push(newUser);
-        fileJson.setUsers(users);
-        res.redirect('/');
-    }, 
-    //Proceso de login
-    loginProcess: (req,res) => { 
-        let userToLogin = user.findByEmail(req.body.email);
-        if(userToLogin) {
-            let isOkThePassword = bcrypt.compareSync(req.body.password, userToLogin.password);
-            if (isOkThePassword) {
-                delete userToLogin.password;
-                req.session.userLogged = userToLogin;
-
-                if(req.body.remember_user) {
-					res.cookie('userEmail', req.body.email, { maxAge: (1000 * 60) * 60 })
-                }
-            } 
+        if (!errores.isEmpty()) {
             return res.render('users/login', {
-                errors: {
-                    email: {
-                        msg: 'Contraseña incorrecta'
+                errores: errores.mapped(),
+                data: req.body
+            })
+        } else {
+
+            db.User.findOne({
+                where: {
+                    email
+                }
+            })
+            .then((user) => {
+                if (user && bcrypt.compareSync(pass.trim(), user.password)) {
+                    req.session.userL = {
+                        id: user.id,
+                        name: user.name,
+                        lastName: user.apellido,
+                        email: user.email,
+                        img: user.avatar,
+                        admin: user.admin
                     }
+                    if (recordar) {
+                        res.cookie('LaEscondida', req.session.userL, {
+                            maxAge: 1000 * 60 * 60 * 24 * 100000
+                        })
+                    }
+                    return res.redirect('/home')
+
+                } else {
+                    return res.render('users/login', {
+                        errores: {
+                            pass: {
+                                msg: 'Credenciales inválidas'
+                            }
+                        },
+                        data: req.body
+
+                    })
                 }
-            });
+            })
+            .catch((error) => {
+                res.send(error)
+            })
         }
-    
-        return res.render('users/login', {
-            errors: {
-                email: {
-                    msg: 'El email ingresado no está registrado'
-                }
-            }
-        });
     },
     logout: (req, res) => {
         res.clearCookie('userEmail');
